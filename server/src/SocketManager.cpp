a1f928877b9e174ea7d3c442099e84ac7b5e45e6
王 天耀 <yao56383702@oumatoMacBook-Pro.local>
2013-02-18 14:16:52 +0800

    tab

--- server/src/SocketManager.cpp
+++ server/src/SocketManager.cpp
@@ -6,23 +6,23 @@ using namespace std;
 
 void sock_info_t::init()
 {
-    m_serv_addr.sin_family = AF_INET;
-    m_serv_addr.sin_addr.s_addr = INADDR_ANY;
-    m_serv_addr.sin_port = htons(6666);
-    m_listen_fd = socket(AF_INET, SOCK_STREAM, 0);
-    int opt = 1;
-    setsockopt(m_listen_fd,SOL_SOCKET,SO_REUSEADDR,&opt,sizeof(opt));
-    bind(m_listen_fd, (struct sockaddr*)&m_serv_addr, sizeof(m_serv_addr));
-    listen(m_listen_fd, 5);
-    m_len = sizeof(m_cli_addr);
+	m_serv_addr.sin_family = AF_INET;
+	m_serv_addr.sin_addr.s_addr = INADDR_ANY;
+	m_serv_addr.sin_port = htons(6666);
+	m_listen_fd = socket(AF_INET, SOCK_STREAM, 0);
+	int opt = 1;
+	setsockopt(m_listen_fd,SOL_SOCKET,SO_REUSEADDR,&opt,sizeof(opt));
+	bind(m_listen_fd, (struct sockaddr*)&m_serv_addr, sizeof(m_serv_addr));
+	listen(m_listen_fd, 5);
+	m_len = sizeof(m_cli_addr);
 	cout << "server init succeed" << endl;
 }
 
 int sock_info_t::accept_cli()
 {
-    int conn_fd = accept(m_listen_fd, (struct sockaddr *)&m_cli_addr, &m_len);
-    cout << "port:" << ntohs(get_cli().sin_port) << endl;
+	int conn_fd = accept(m_listen_fd, (struct sockaddr *)&m_cli_addr, &m_len);
+	cout << "port:" << ntohs(get_cli().sin_port) << endl;
 	log::log_current_time();
 
-    return conn_fd;
+	return conn_fd;
 }
